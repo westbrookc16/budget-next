c8221a7c554ece773e1474ec4c258aff3caac959
Chris Westbrook <westbchris@gmail.com>
2024-03-01 22:47:58 -0500

    added error handling ot api call.

--- app/api/create-portal-link/[customerId]/route.ts
+++ app/api/create-portal-link/[customerId]/route.ts
@@ -1,13 +1,19 @@
+import * as sentry from "@sentry/nextjs";
 import { redirect } from "next/navigation";
 import Stripe from "stripe";
 import { stripe } from "@/utils/stripe/config";
 
 export async function GET(req: Request, { params }: any) {
-  const { customerId } = params;
-  const link = await stripe.billingPortal.sessions.create({
-    customer: customerId,
-    return_url: `${process.env.VERCEL_URL as string}`,
-  });
+  try {
+    const { customerId } = params;
+    const link = await stripe.billingPortal.sessions.create({
+      customer: customerId,
+      return_url: `${process.env.VERCEL_URL as string}`,
+    });
 
-  return new Response(JSON.stringify({ url: link.url }), { status: 200 });
+    return new Response(JSON.stringify({ url: link.url }), { status: 200 });
+  } catch (error) {
+    sentry.captureException(error);
+    return redirect("/error");
+  }
 }
