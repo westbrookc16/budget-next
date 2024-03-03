97726b29b9b5dea2b6d5b5a92269361970da37f8
Chris Westbrook <westbchris@gmail.com>
2024-03-03 13:15:09 -0500

    added try/catch to api call.

--- app/api/users/getSubscriptionStatus/[userid]/route.ts
+++ app/api/users/getSubscriptionStatus/[userid]/route.ts
@@ -1,10 +1,16 @@
 import prisma from "@/utils/prisma";
+import * as sentry from "@sentry/nextjs";
 export async function GET(req: Request, { params }: any) {
-  const { userid } = params;
-  const user = await prisma?.subscriptions.findFirst({
-    where: { user_id: userid },
-  });
-  const subscriptionStatus = user?.status ?? "none";
-  const customerId = user?.customer ?? "";
-  return new Response(JSON.stringify({ subscriptionStatus, customerId }));
+  try {
+    const { userid } = params;
+    const user = await prisma?.subscriptions.findFirst({
+      where: { user_id: userid },
+    });
+    const subscriptionStatus = user?.status ?? "none";
+    const customerId = user?.customer ?? "";
+    return new Response(JSON.stringify({ subscriptionStatus, customerId }));
+  } catch (error) {
+    sentry.captureException(error);
+    return new Response("Error", { status: 500 });
+  }
 }
