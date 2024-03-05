7485f968b8e92fa551dbb9d98d39708795d75657
Chris Westbrook <westbchris@gmail.com>
2024-03-04 20:35:05 -0500

    Changed direct

--- app/onboarding/layout.tsx
+++ app/onboarding/layout.tsx
@@ -1,19 +1,23 @@
-import { auth } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
-import React from 'react'
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   // Check if a user has completed onboarding
   // If yes, redirect them to /dashboard
   const sessionClaims = auth().sessionClaims;
-  console.log(`onboardingComplete: ${sessionClaims?.metadata?.onboardingComplete}`)
-  if (sessionClaims && sessionClaims.metadata.onboardingComplete=== true) {
-    console.log("onboarding complete, redirecting to dashboard")
-    redirect('/')
+  console.log(
+    `onboardingComplete: ${sessionClaims?.metadata?.onboardingComplete}`
+  );
+  if (sessionClaims && sessionClaims.metadata.onboardingComplete === true) {
+    console.log("onboarding complete, redirecting to dashboard");
+    redirect(`${process.env.NEXT_PUBLIC_BASE_URL}`);
+  } else {
+    console.log("onboarding not complete yet");
   }
-else{
-console.log("onboarding not complete yet")
-
-}
-  return <>{children}</>
+  return <>{children}</>;
 }
