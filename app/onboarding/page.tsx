0cc4a9bbd8b175327e64b94dd474a315f04ecce0
Chris Westbrook <westbchris@gmail.com>
2024-03-04 19:49:36 -0500

    Fixed onboarding page.

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,24 +1,24 @@
-'use client';
+"use client";
 import { useRouter } from "next/navigation";
 import { completeOnboarding } from "../actions/onboarding";
-import { useUser } from '@clerk/nextjs'
+import { useUser } from "@clerk/nextjs";
 export default function Onboarding() {
-    const { user } = useUser();
-    const router = useRouter()
-    return (
-        <div>
-            <h1>Onboarding</h1>
-            <form
-                action={async (data:FormData) => {
-                    const res = await completeOnboarding(data);
-                    console.log(res);
-                    await user?.reload();
-                    router.push("/");
-                }}
-            >
-                Hi, <b>{user?.firstName}</b>, thanks for stopping by.
-                <button type="submit">Complete Onboarding</button>
-            </form>
-        </div>
-    );
-            }
+  const { user } = useUser();
+  const router = useRouter();
+  return (
+    <div>
+      <h1>Onboarding</h1>
+      <form
+        action={async (data: FormData) => {
+          const res = await completeOnboarding(data);
+          console.log(res);
+          await user?.reload();
+          router.push(`${process.env.NEXT_PUBLIC_BASE_URL}`);
+        }}
+      >
+        Hi, <b>{user?.firstName}</b>, thanks for stopping by.
+        <button type="submit">Complete Onboarding</button>
+      </form>
+    </div>
+  );
+}
