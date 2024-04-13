ed4dbac96d4d10c4efe9d2e1b7cfe268823274ef
Chris Westbrook <westbchris@gmail.com>
2024-04-13 18:45:49 -0400

    Fixed middleware to ignored routes.

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // for more information about configuring your Middleware
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
-  publicRoutes: ["/", "/api/webhooks", "/api/clerk/webhooks"],
+  ignoredRoutes: ["/", "/api/webhooks", "/api/clerk/webhooks"],
 });
 
 export const config = {
