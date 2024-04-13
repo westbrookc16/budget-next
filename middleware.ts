513cad9c9355a2a94801a90819d7c0f7977f1ac1
Chris Westbrook <westbchris@gmail.com>
2024-04-13 18:31:32 -0400

    Fixed middleware hopefullly.

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,18 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/api/clerk/webhooks",
-  "/api/webhooks",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) return; // if it's a public route, do nothing
-  auth().protect(); // for any other route, require auth
+// See https://clerk.com/docs/references/nextjs/auth-middleware
+// for more information about configuring your Middleware
+export default authMiddleware({
+  // Allow signed out users to access the specified routes:
+  publicRoutes: ["/", "/api/webhooks", "/api/clerk/webhooks"],
 });
+
 export const config = {
-  debug: true,
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Exclude files with a "." followed by an extension, which are typically static files.
+    // Exclude files in the _next directory, which are Next.js internals.
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    // Re-include any files in the api or trpc folders that might have an extension
+    "/(api|trpc)(.*)",
+  ],
 };
