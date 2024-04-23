f7d86e8566b85348f7deb1e7561b3800300f119d
Chris Westbrook <westbchris@gmail.com>
2024-04-22 23:57:17 -0400

    Fixed base url redirection.

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -10,6 +10,7 @@ export async function GET(request: NextRequest) {
   const next = searchParams.get("next") ?? "/";
 
   const redirectTo = request.nextUrl.clone();
+
   redirectTo.pathname = next;
   redirectTo.searchParams.delete("token_hash");
   redirectTo.searchParams.delete("type");
@@ -23,7 +24,7 @@ export async function GET(request: NextRequest) {
     });
     if (!error) {
       redirectTo.searchParams.delete("next");
-      return NextResponse.redirect(redirectTo);
+      return NextResponse.redirect(process.env.NEXT_PUBLIC_BASE_URL);
     }
   }
 
